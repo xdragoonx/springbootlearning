e2eeeef5ed076edfbab948b16509c788c2c75ece
Grzegorz Puchala <dragoon.mx@gmail.com>
2019-02-08 12:52:50 +0100

    Init

---
+++ src/main/java/test/tutorial/dragoon/springboot/learningspringboot/LearningSpringbootApplication.java
@@ -0,0 +1,16 @@
+package test.tutorial.dragoon.springboot.learningspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringbootApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(LearningSpringbootApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
+
---
+++ src/main/java/test/tutorial/dragoon/springboot/learningspringboot/controller/TestController.java
@@ -0,0 +1,17 @@
+package test.tutorial.dragoon.springboot.learningspringboot.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import test.tutorial.dragoon.springboot.learningspringboot.InitDto;
+
+
+@RestController
+public class TestController {
+
+    @GetMapping("/first-one")
+    @ResponseStatus(HttpStatus.OK)
+    public InitDto get() {
+        return new InitDto(true, 1, "first string");
+    }
+
+}
