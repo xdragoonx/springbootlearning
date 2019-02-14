4679d48e1170bd1682cd3b1ed8f41c459c8a6b8c
Grzegorz Puchala <dragoon.mx@gmail.com>
2019-02-14 10:57:31 +0100

    Added integration tests handling

---
+++ src/test/java/test/tutorial/dragoon/springboot/learningspringboot/domain/handler/UserCreationHandlerIT.java
@@ -0,0 +1,25 @@
+package test.tutorial.dragoon.springboot.learningspringboot.domain.handler;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class UserCreationHandlerIT {
+
+    @Autowired
+    private UserCreationHandler handler;
+
+    @Test
+    @DisplayName("TBD")
+    void testCreation() {
+        assertTrue(true);
+    }
+
+}
